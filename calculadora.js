50c9ea4a41e28397e761a2d30973da6c5a83ee7d
Edelson Santos <edelsonjr.4@hotmail.com>
2024-08-29 22:41:42 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,17 +1,12 @@
 const prompt = require('prompt-sync')();
 
 
-function lerPartidas(vitorias, derrotas) {
-    let vitorias = prompt("Digite a quantidade de vitórias: ")
-    let derrotas = prompt("Digite a quantidade de derrotas: ")
-}
+let vitorias = prompt('Digite a quantidade de vitórias: ')
+let derrotas = prompt('Digite a quantidade de derrotas: ')
 
 
-function calcularPartida(vitorias, derrotas) {
-    saldo = vitorias - derrotas
 
-    return saldo
-}
 
-partidas = calcularPartida()
+
+
 
