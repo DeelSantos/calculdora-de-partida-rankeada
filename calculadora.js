2020e00a4b6df335c0dc7a061efe4653c77ffb47
Edelson Santos <edelsonjr.4@hotmail.com>
2024-08-30 20:47:20 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -21,20 +21,20 @@ function lerPartidas() {
 saldoVitorias = lerPartidas();
 
 if(saldoVitorias<= 10) {
-    console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivelFerro}`);
+    console.log(`O Herói tem saldo de ${saldoVitorias}pts e está no nível de ${nivelFerro}`);
     
 } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
-    console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivelBronze}`);
+    console.log(`O Herói tem saldo de ${saldoVitorias}pts e está no nível de ${nivelBronze}`);
 } else if(saldoVitorias > 20 && saldoVitorias <= 50) {
-    console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivelPrata}`);
+    console.log(`O Herói tem saldo de ${saldoVitorias}pts e está no nível de ${nivelPrata}`);
 } else if (saldoVitorias > 50 && saldoVitorias <= 80) {
-    console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivelOuro}`);
+    console.log(`O Herói tem saldo de ${saldoVitorias}pts e está no nível de ${nivelOuro}`);
 } else if (saldoVitorias > 80 && saldoVitorias <= 90) {
-    console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivelDiamante}`);
+    console.log(`O Herói tem saldo de ${saldoVitorias}pts e está no nível de ${nivelDiamante}`);
 } else if (saldoVitorias > 90 && saldoVitorias <= 100) {
-    console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivelLendario}`);
+    console.log(`O Herói tem saldo de ${saldoVitorias}pts e está no nível de ${nivelLendario}`);
 } else {
-    console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivelImortal}`);
+    console.log(`O Herói tem saldo de ${saldoVitorias}pts e está no nível de ${nivelImortal}`);
 }
     
     
