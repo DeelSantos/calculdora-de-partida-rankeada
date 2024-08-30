659c234cd2b8e90b73758ad1e8d97333624374af
Edelson Santos <edelsonjr.4@hotmail.com>
2024-08-29 22:32:28 -0300

    aaa

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,10 @@
+const prompt = require('prompt-sync')();
+
+
+function lerPartidas(vitorias, derrotas) {
+    let vitorias = prompt("Digite a quantidade de vitórias: ")
+    let derrotas = prompt("Digite a quantidade de derrotas: ")
+}
 
 
 function calcularPartida(vitorias, derrotas) {
